bdd834be27a1203fed6288ee379bdb0fb1213c9f
SmitasCodes <airidassmith30@gmail.com>
2024-11-17 23:37:44 +0200

    added some changes to input form and made some progress towards getting exercises

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -6,13 +6,37 @@ const Exercises = () => {
   const [showTemplate, setShowTemplate] = useState(false);
   const [showExerciseAddBtn, setShowExerciseAddBtn] = useState(false);
 
+  useEffect(() => {
+    getExercises();
+  }, []);
+
+  const getExercises = () => {
+    axios
+      .get(
+        "http://localhost:8787/api/workout-template/6735d7699ebaa1f74f6a19e5/exercises",
+        {
+          headers: {
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3MjkzZDkzMWQ1YzgxZDE4NzM3ZGUwNSIsImlhdCI6MTczMDkwMjExMSwiZXhwIjoxNzMzNDk0MTExfQ.pwyD88jbQQX6aEoOsHp6qUKBYS-X2DZhso1ey44HrEw",
+          },
+        }
+      )
+      .then((res) => {
+        const exercises = res.data.exercises;
+        setExercises(exercises);
+
+        console.log(exercises);
+      });
+  };
+
   const handleAddTemplateClick = () => {
     setShowTemplate(!showTemplate);
   };
 
   const renderAddExerciseInput = () => {
-    const addAddExerciceButton = (e) => {
+    const addExerciceButton = (e) => {
       if (e.target.value) {
+        console.log("ss");
         setShowExerciseAddBtn(true);
       } else {
         setShowExerciseAddBtn(false);
@@ -22,19 +46,19 @@ const Exercises = () => {
     return (
       <div className="flex bg-red-900 justify-between">
         <label>Exercise name:</label>
-        <input type="text" onKeyUp={addAddExerciceButton} />
-        {showExerciseAddBtn && (
-          <button
-            className={`bg-red-500 px-1 ${
-              showExerciseAddBtn ? "opacity" : "0"
-            }`}
-          >
-            Add
-          </button>
-        )}
+        <input type="text" onKeyUp={addExerciceButton} />
+        <button
+          className={`bg-red-500 px-1 ${
+            showExerciseAddBtn ? "opacity-1" : "opacity-0"
+          }`}
+        >
+          Add
+        </button>
       </div>
     );
   };
+
+  const renderExercisesList = () => {};
 
   return (
     <div className="bg-red-300 w-96 h-96 m-auto">
@@ -44,7 +68,12 @@ const Exercises = () => {
       >
         Add template
       </button>
-      {showTemplate && <div>{renderAddExerciseInput()}</div>}
+      {showTemplate && (
+        <div>
+          {renderExercisesList}
+          {renderAddExerciseInput()}
+        </div>
+      )}
     </div>
   );
 };
