28750c31fdea25ad7d7f4d2b6b54db61eecaf7a5
SmitasCodes <airidassmith30@gmail.com>
2024-11-18 17:48:54 +0200

    create some basic design and templates component doesnt look that bad, also added icons

--- src/components/Templates/WorkoutTemplates.jsx
+++ src/components/Templates/WorkoutTemplates.jsx
@@ -20,20 +20,22 @@ const WorkoutTemplates = () => {
   }, []);
 
   return (
-    <div className="bg-red-300 w-96 h-96 m-auto">
+    <div className="bg-red-300 w-96 h-96 m-auto relative">
+      <h2 className="text-center py-2">Templates</h2>
       <ul>
         {templates.map((template) => {
           return (
             <li className="bg-red-400 mb-2 px-3 flex justify-between ">
               {template.name}
               <div className="flex items-center">
-                <CiEdit className="mr-2"/>
-                <MdDelete />
+                <CiEdit className="mr-2 cursor-pointer" title="edit"/>
+                <MdDelete className="cursor-pointer" title="delete"/>
               </div>
             </li>
           );
         })}
       </ul>
+      <button className="px-4 py-1 bg-red-500 rounded-2xl absolute bottom-2 left-1/2  transform -translate-x-1/2">Add template</button>
     </div>
   );
 };
