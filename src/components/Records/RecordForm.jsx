18089efbe11f7c000f64baa67bf412131ff213d3
SmitasCodes <airidassmith30@gmail.com>
2024-12-12 12:44:16 +0200

    made correct selectedTemplate structure

--- src/components/Records/RecordForm.jsx
+++ src/components/Records/RecordForm.jsx
@@ -6,11 +6,31 @@ const RecordForm = () => {
   const [selectedTemplate, setSelectedTemplate] = useState(null);
   const { templates } = useTemplates();
 
+  const setsOptions = Array.from({ length: 10 }, (_, i) => i + 1);
+
   useEffect(() => {
-    setSelectedTemplate(
-      templates.find((template) => template._id == selectTemplate)
+    const searchTemplate = templates.find(
+      (template) => template._id == selectTemplate
     );
+
+    if (searchTemplate) {
+      const transformedExercises = searchTemplate.exercises.map((exercise) => ({
+        ...exercise,
+        weight: 0,
+        sets: 0,
+        reps: 0,
+      }));
+
+      const result = {
+        template_id: searchTemplate._id,
+        exercises: transformedExercises,
+      };
+
+      setSelectedTemplate(result);
+    }
   }, [selectTemplate]);
+
+  console.log(selectedTemplate);
 
   return (
     <form>
@@ -34,10 +54,29 @@ const RecordForm = () => {
       </div>
       {selectedTemplate ? (
         <div>
-          <label>Your exercises: </label>
-          {selectedTemplate.exercises.map((template) => {
-            return <h2>{template.exercise_name}</h2>;
-          })}
+          <h1>Your exercises:</h1>
+          <ul className="px-2">
+            {selectedTemplate.exercises.map((template) => {
+              return (
+                <li className="flex py-1">
+                  <h2 className="mr-4">{template.exercise_name}</h2>
+                  <label>Sets:</label>
+                  <select>
+                    <option defaultValue="" disabled selected>
+                      0
+                    </option>
+                    {setsOptions.map((set) => {
+                      return (
+                        <option value={set} key={set}>
+                          {set}
+                        </option>
+                      );
+                    })}
+                  </select>
+                </li>
+              );
+            })}
+          </ul>
         </div>
       ) : (
         ""
